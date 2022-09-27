2da0543ba6fb300fbcae0efae53c3fcdc92f26c9
Lisha Liang <lishaliang1999@gmail.com>
2022-09-27 20:25:05 +0000

    joinning query

---
+++ 2_queries_joins/2_cohort_total_assignment_duration.sql
@@ -0,0 +1,4 @@
+SELECT SUM(assignment_submissions.duration) as time
+FROM assignment_submissions 
+JOIN students ON student_id = students.id JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12';
